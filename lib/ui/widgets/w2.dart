4db6bf0d2d57c4888c3177cf6e56f6db2eca284f
jgayon <79709433+jgayon@users.noreply.github.com>
2025-03-07 14:34:53 -0500

    Update w2.dart

--- lib/ui/widgets/w2.dart
+++ lib/ui/widgets/w2.dart
@@ -1,22 +1,27 @@
+import 'package:f_getxstate_demo/controllers/Countercontroller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
 class W2 extends StatelessWidget {
   const W2({super.key});
 
   @override
   Widget build(BuildContext context) {
+    CountController controller = Get.find();
     return Container(
       color: Colors.deepOrange,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Value'),
+          Obx(()=>Text('${controller.value1}')),
           ElevatedButton(
               key: const Key('resetButton'),
-              onPressed: null,
+              onPressed: () => controller.reset(),
               child: Text('Reset'))
         ],
       ),
     );
   }
 }
+
