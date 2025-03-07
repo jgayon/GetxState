685b3e4a7c399d7a7575faf12cc4f426c4677a6d
jgayon <79709433+jgayon@users.noreply.github.com>
2025-03-07 14:35:07 -0500

    Update w3.dart

--- lib/ui/widgets/w3.dart
+++ lib/ui/widgets/w3.dart
@@ -1,16 +1,20 @@
+import 'package:f_getxstate_demo/controllers/Countercontroller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
 class W3 extends StatelessWidget {
   const W3({super.key});
 
   @override
   Widget build(BuildContext context) {
+    CountController controller = Get.find();
     return Container(
       color: Colors.yellowAccent,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Value'),
+          Obx(()=>Text('${controller.value1}')),
         ],
       ),
     );
