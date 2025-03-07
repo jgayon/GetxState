fb3f0b4f8dfe63ba08fa8e11eead065b69c159c0
jgayon <79709433+jgayon@users.noreply.github.com>
2025-03-07 14:34:33 -0500

    Update w1.dart

--- lib/ui/widgets/w1.dart
+++ lib/ui/widgets/w1.dart
@@ -1,19 +1,22 @@
+import 'package:f_getxstate_demo/controllers/Countercontroller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class W1 extends StatelessWidget {
   const W1({super.key});
 
   @override
   Widget build(BuildContext context) {
+    CountController controller = Get.find();
     return Container(
       color: Colors.amber,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Value'),
+          Obx(()=>Text('${controller.value1}')),
           ElevatedButton(
               key: const Key('incrementButton'),
-              onPressed: null,
+              onPressed: () => controller.increase(),
               child: Text('Increment'))
         ],
       ),
